a54019cc463215818c313d5c5d3548e6e89bda1f
Scott Zimmerman <bighead.0@gmail.com>
2025-07-26 07:28:47 -0400

    Add spin-slash damage type to final boss Ganon form.

--- InGame/GameObjects/Bosses/BossFinalBoss.cs
+++ InGame/GameObjects/Bosses/BossFinalBoss.cs
@@ -1680,7 +1680,7 @@ namespace ProjectZ.InGame.GameObjects.Bosses
             }
 
             // ganon damage form
-            if (_ganonForm && !_aiDamageState.IsInDamageState() && (damageType & HitType.PegasusBootsSword) != 0)
+            if (_ganonForm && !_aiDamageState.IsInDamageState() && (((damageType & HitType.PegasusBootsSword) != 0) || (damageType & HitType.SwordSpin) != 0))
             {
                 _ganonLives -= damage;
                 if (_ganonLives <= 0)
